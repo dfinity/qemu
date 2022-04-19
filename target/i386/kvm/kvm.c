c7b3196620c02e72bdd995b72b61a54a43291cf0
Michael Roth <michael.roth@amd.com>
2022-04-18 23:07:56 -0500

    *i386/sev: Add handling for KVM_EXIT_VMGEXIT
    Currently, when UPM is enabled, the kernel will forward VMGEXITs for
    page-state changes to userspace so that it can do the appropriate
    allocation/deallocation for the private memory FD. Add handling for that
    here.
    
    For now it does make sense to at least handle the GHCB-based page-state
    change requests, since it allows for up to 253 ranges to batched in a
    single exit, whereas KVM_EXIT_MEMORY_FAULT has no support for multiple
    ranges in it's current incarnation.
    
    If this does continue to be handled via KVM_EXIT_VMGEXIT, then GHCB
    definitions will want to be shared with the kernel ones. For now just
    define the bits that are needed locally.
    
    Signed-off-by: Michael Roth <michael.roth@amd.com>

--- target/i386/kvm/kvm.c
+++ target/i386/kvm/kvm.c
@@ -5447,6 +5447,9 @@ int kvm_arch_handle_exit(CPUState *cs, struct kvm_run *run)
     case KVM_EXIT_HYPERCALL:
         ret = kvm_handle_exit_hypercall(cpu, run);
         break;
+    case KVM_EXIT_VMGEXIT:
+        ret = kvm_handle_vmgexit(&run->vmgexit.ghcb_msr, &run->vmgexit.error);
+        break;
     default:
         fprintf(stderr, "KVM: unknown exit reason %d\n", run->exit_reason);
         ret = -1;
